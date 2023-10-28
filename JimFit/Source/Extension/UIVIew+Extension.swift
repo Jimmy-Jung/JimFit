195f14713c8d6864274f0fecfa100dc187efc32a
Jimmy <joony300@gmail.com>
2023-10-29 01:01:03 +0900

    [Fix] 다크모드 애니메이션 효과

--- JimFit/Source/Extension/UIVIew+Extension.swift
+++ JimFit/Source/Extension/UIVIew+Extension.swift
@@ -39,3 +39,22 @@ extension UIView {
         return result
     }
 }
+
+extension UIView {
+    func getKeyWindow() -> UIWindow? {
+        return self.window
+    }
+    func makeSnapshot() -> UIImage? { return getKeyWindow()?.layer.makeSnapshot() }
+}
+
+extension CALayer {
+    func makeSnapshot() -> UIImage? {
+        let scale = UIScreen.main.scale
+        UIGraphicsBeginImageContextWithOptions(frame.size, false, scale)
+        defer { UIGraphicsEndImageContext() }
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+        render(in: context)
+        let screenshot = UIGraphicsGetImageFromCurrentImageContext()
+        return screenshot
+    }
+}
